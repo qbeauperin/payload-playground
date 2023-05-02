c6fc5782c5c6e25a40b01daee7494aae17ce6527
Quentin Beauperin <quentin.beauperin@gmail.com>
2023-05-01 17:09:45 -0700

    fix: move plugin out of its folder, add types and safety checks

--- src/plugins/triggerDeploy.ts
+++ src/plugins/triggerDeploy.ts
@@ -1,11 +1,32 @@
 import { Config } from 'payload/config';
-import { PluginOptions } from './types';
+import { AfterDeleteHook } from 'payload/dist/collections/config/types';
+import { AfterChangeHook } from 'payload/dist/globals/config/types';
 
+export interface PluginOptions {
+    collections: Array<string>;
+    webhook: string | undefined; // can be undefined if using a env var and this codes runs in the client
+}
+
+/**
+ * Adds hooks to all specified collections that will call a webhook whenever a doc is created, updated, or deleted.
+ * 
+ * If drafts are enabled, the webhook will only be called when the doc status is or was `published`.
+ * 
+ * @param pluginOptions.collections slugs of the collections that will trigger the webhook
+ * @param pluginOptions.webhook url that will be called when doc is created, updated, or deleted
+ *
+ */
 export const triggerDeploy = (pluginOptions: PluginOptions) => (incomingConfig: Config): Config => {
+
+    // Make sure that collections have been defined
+    if (!incomingConfig?.collections) return incomingConfig;
+
+    // Make sure the webhook is present
+    if (!pluginOptions.webhook) return incomingConfig;
 
     const trigger = () => {
         try {
-            fetch(pluginOptions.webhook, { method: 'POST' })
+            fetch(pluginOptions.webhook as string, { method: 'POST' })
                 .then((response) => {
                     return response.status === 200;
                 })
@@ -14,12 +35,12 @@ export const triggerDeploy = (pluginOptions: PluginOptions) => (incomingConfig: 
             return false;
         }
     }
-    
+
     const config: Config = {
         ...incomingConfig,
         collections: [
             ...incomingConfig.collections.map((collection) => {
-                
+
                 // If collection is not part of the ones passed in the options, ignore it
                 if (!pluginOptions?.collections.includes(collection.slug)) return collection;
 
@@ -27,15 +48,15 @@ export const triggerDeploy = (pluginOptions: PluginOptions) => (incomingConfig: 
                 const draftsEnabled = typeof collection?.versions === 'object' && collection?.versions?.drafts;
 
                 // Define hooks
-                const onAfterChange = async ({ doc, previousDoc }) => {
-                    if (!draftsEnabled || doc._status === 'published' || previousDoc?._status === 'published'){
+                const onAfterChange: AfterChangeHook = async ({ doc, previousDoc }) => {
+                    if (!draftsEnabled || doc._status === 'published' || previousDoc?._status === 'published') {
                         trigger();
                     }
                 }
-                const onAfterDelete = async () => {
+                const onAfterDelete: AfterDeleteHook = async () => {
                     trigger();
                 }
-            
+
                 // Make sure to spread already defined hooks
                 const afterChange = collection?.hooks?.afterChange ? [
                     ...collection.hooks.afterChange,
--- src/plugins/triggerDeploy/types.ts
+++
@@ -1,4 +0,0 @@
-export interface PluginOptions {
-    collections: Array<string>;
-    webhook: string;
-}
